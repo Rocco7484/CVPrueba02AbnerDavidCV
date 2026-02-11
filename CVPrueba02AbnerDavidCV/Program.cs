69f5dc3df145f7e0c4a28bf82374ef1df80e800c
Alumno <Alumno@DAM1-PC2>
2026-02-11 08:31:05 +0100

    Agregar archivos de proyecto.

---
+++ CVPrueba02AbnerDavidCV/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CVPrueba02AbnerDavidCV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
